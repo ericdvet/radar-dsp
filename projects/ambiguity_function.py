46f9df6eedb01756e3ed0a22f35711504e300981
ericdvet <ericdvet@gmail.com>
2025-08-28 18:50:50 -0700

    ambiguity function for simple pulse

---
+++ projects/ambiguity_function.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 28 14:05:00 2025
+
+This project sees the exploration of the ambiguity function for different
+waveforms:
+    - Write a function to compute and plot the ambiguity function for different
+        radar waveforms:
+        - Rectangular pulse
+        - Linear frequency modulated (LFM) chirp
+        - Phase-coded waveform (e.g, Barker code)
+    - Compare range-Doppler resolution trade-offs.
+    - Discuss ambiguity sidelobes and how waveform design impacts them.
+
+Concepts used: waveform analysis, range-Doppler coupling, ambiguity functions.
+
+@author: ericdvet
+"""
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import numpy as np
+import scipy.constants as const
+import matplotlib.pyplot as plt
+import scipy.fft as fft
+from mpl_toolkits.mplot3d import Axes3D  
+
+from radardsp.waveforms.waveforms import SquarePulse
+from radardsp.waveforms.tools import matched_filter
+from radardsp.tools import db2linear, AWGN
+
+
+def main():
+
+    f = 1e5        # Carrier frequency
+    sampling_frequency = f*32
+    
+    sampling_frequency = sampling_frequency
+    period = 1 / sampling_frequency
+
+    tau = 1e-4                              # Pulse length 
+    times = np.arange(-tau, tau, period)    # |tau| includes entire pulse
+    n = len(times)
+    
+    square_pulse = SquarePulse(n = n, tau = tau, f = f)
+    x = square_pulse.waveform(times)                    # Transmitted waveform
+    N = np.size(x)
+    
+    A, f_d = ambiguity_function(x = x, tau = tau, times = times)
+    
+    plot_ambiguity_function(A = A, times = times, f_d = f_d, 
+                            waveform_name = "Simple Pulse")
+                  
+def ambiguity_function(x : np.ndarray, tau : float, times : np.ndarray):
+    """
+    Generate ambiguity function for waveform.
+
+    Parameters
+    ----------
+    x : np.ndarray
+        Waveform x(t).
+    tau : float
+        Sampling frequency.
+    times : np.ndarray
+        Time samples of x(t).
+
+    Returns
+    -------
+    A : np.ndarray
+        Ambiguity function.
+    f_d : np.ndarray
+        Frequency axis. Returned for plotting purposes.
+
+    """
+    
+    f_d = np.linspace(-10/tau, 10/tau, np.size(times))  # To make the figures
+                                                        # identical to Richards
+                                                    
+    N = np.size(x)
+    A = np.zeros((N, N), dtype=complex)     # Ambiguity function initialized
+    
+    s = times
+    ds = s[1] - s[0]            
+    
+    for t_i, tau in enumerate(times):
+        shift = int(round(tau / ds))        # tau / ds = index to shift by
+        x_shifted = np.conj(shift_signal(x, shift))
+        for f_i, fd in enumerate(f_d):
+            A[t_i, f_i] = np.sum(   # Richards 2005, Equation 4.30
+                x * x_shifted * np.exp(-1j * 2*np.pi * fd * s)) * ds
+            
+    return A, f_d
+        
+
+    
+def plot_ambiguity_function(A : np.ndarray, times : np.ndarray, f_d,
+                            waveform_name : str):
+    """
+    Plot ambiguity function
+
+    Parameters
+    ----------
+    A : np.ndarray
+        x(t).
+    times : np.ndarray
+        t.
+    f_d : TYPE
+        F_d.
+    waveform_name : str
+        Waveform name.
+
+    Returns
+    -------
+    None.
+
+    """
+    fig = plt.figure(figsize=(10,6))
+    ax = fig.add_subplot(111, projection='3d')
+    
+    Tau, FD = np.meshgrid(times, f_d, indexing='ij')  # Create meshgrid
+    ax.plot_surface(Tau, FD, np.abs(A), cmap='inferno')
+    
+    ax.set_xlabel('Delay [s]')
+    ax.set_ylabel('Doppler mismatch [Hz]')
+    ax.set_zlabel('|A(t, F_D)|')
+    ax.set_title(f'{waveform_name} Ambiguity Function')
+    
+    plt.show()
+    
+def shift_signal(x : np.ndarray, shift : int):
+    """
+    Shift a signal. Unlike a normal shift that could be accomplished with
+    something like numpy.roll(), this ensures that the shift is 
+    pre and post logged with zeros.
+
+    Parameters
+    ----------
+    x : np.ndarray
+        Signal to be shifted.
+    shift : int
+        Amount to be shifted.
+
+    Returns
+    -------
+    x_shifted : np.ndarray
+        Shifted signal.
+
+    """
+    N = len(x)
+    x_shifted = np.zeros(N, dtype=complex)
+    if shift > 0:
+        x_shifted[shift:] = (x[:N-shift])
+    elif shift < 0:
+        x_shifted[:N+shift] = (x[-shift:])
+    else:
+        x_shifted = (x)
+    return x_shifted
+
+if __name__ == "__main__":
+    main()
